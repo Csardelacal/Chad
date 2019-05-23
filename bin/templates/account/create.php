4aa520fdf3e7cd31f7686865d471c44272ed8c01
cesar <cesar@magic3w.com>
2019-05-23 17:06:32 +0200

    Working on simplfying the UI and make the whole appearance more streamlined

--- bin/templates/account/create.php
+++ bin/templates/account/create.php
@@ -1,21 +1,56 @@
 
-<div class="heading topbar sticky">
-	Create new account
+<div class="spacer" style="height: 20px"></div>
+
+<div class="row l1">
+	<div class="span l1">
+		<div class="heading topbar sticky">
+			Create new account
+		</div>
+	</div>
 </div>
 
-<div class="spacer" style="height: 30px"></div>
+<div class="spacer" style="height: 20px"></div>
 
 <div class="row l3">
 	<div class="span l2">
 		<form method="POST" action="" class="regular">
-			<div class="field">
-				<label for="name">Name:</label>
-				<input type="text" name="name" id="name">
-			</div>
-			
-			<div style="text-align: right">
-				<input type="submit" value="Create account">
+			<div class="row l3">
+				<div class="span l2">
+					<div class="field">
+						<input type="text" name="name" placeholder="Name your account" id="name">
+					</div>
+
+				</div>
+				<div class="span l1">
+					<input type="submit" value="Create account">
+				</div>
 			</div>
 		</form>
+
+		<div class="spacer" style="height: 20px"></div>
+
+		<div class="row l1">
+			<div class="span l1">
+				<p class="secondary">
+					Assign a name to your account, this will allow you to identify your account
+					when adding funds, requesting funds or transferring money to other users.
+				</p>
+				<p class="secondary">
+					Unlike bank accounts, our accounts are just small virtual spaces 
+					to make it easier to manage your funds across different applications
+					and also allowing you to define quotas for certain applications.
+					The use of these accounts is restricted to applications that we 
+					own and Chad does not provide any sort of financial service - this 
+					application is not allowed to sell financial advise, loans or credit
+					or any similar service.
+					The funds may only be used to spend money on services directly provided
+					by the platform owner, and not on third party stores or services.
+				</p>
+				<p class="secondary">
+					Whenever you feel unsafe or feel like you need help, contact the 
+					system administrator.
+				</p>
+			</div>
+		</div>
 	</div>
 </div>
